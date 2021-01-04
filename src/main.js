1563983cb6e9439377e76f0246bcd9878a20d1f3
David Huang <mswong@nexify.com.hk>
2021-01-04 16:35:29 +0800

    i18n test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
+import zh from './i18n/zh' 
+import en from './i18n/en' 
+
+Vue.use(VueI18n)
+
+let locale = 'en';
 
 Vue.config.productionTip = false
 
+const i18n = new VueI18n({
+  locale: locale,
+  fallbackLocale: 'en',
+  messages: {
+    'zh': zh,
+    'en': en
+  }
+});
+
 new Vue({
   render: h => h(App),
+  i18n
 }).$mount('#app')
